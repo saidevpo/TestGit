a47c62c10376185eb11836c40a1a17bce191bdfd
saidevpo <saidev.polavarapu@gmail.com>
2016-11-15 11:44:25 -0500

    Commit

---
+++ TestGIT/src/testing/FirstClass.java
@@ -0,0 +1,11 @@
+package testing;
+
+public class FirstClass {
+
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Test one");
+		
+	}
+}
---
+++ TestGIT/src/testing/TestTwo.java
@@ -0,0 +1,10 @@
+package testing;
+
+public class TestTwo {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Second test");
+	}
+
+}
