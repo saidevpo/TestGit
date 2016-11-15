a9bb2c551a89e65407920df5b396e448aaa75eaa
saidevpo <saidev.polavarapu@gmail.com>
2016-11-15 12:24:28 -0500

    Both files edited with messsages

--- TestGIT/src/testing/FirstClass.java
+++ TestGIT/src/testing/FirstClass.java
@@ -6,6 +6,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		
 		System.out.println("Test one");
+		System.out.println("Test One edit");
 		
 	}
 }
--- TestGIT/src/testing/TestTwo.java
+++ TestGIT/src/testing/TestTwo.java
@@ -5,6 +5,7 @@ public class TestTwo {
 	public static void main(String[] args) {
 		
 		System.out.println("Second test");
+		System.out.println("Test two edit");
 	}
 
 }
